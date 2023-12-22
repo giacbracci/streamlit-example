a574345feb915287a67195174f1199f289f0c5ea
giacbracci <154382061+giacbracci@users.noreply.github.com>
2023-12-22 19:34:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title("FEF Academy Stock Price Fair Value Calculator")
 
 st.write("### Input Data")
 col1, col2 = st.columns(2)
-rev = col1.number_input("2023 Revenues (bn $)", min_value=0.0, format="%.2f", value=228.96)
+rev = col1.number_input("2023 Revenues (billion $)", min_value=0.0, format="%.2f", value=228.96)
 fcf = col1.number_input("2023 Free Cash Flow (billion $)", min_value=0.0, format="%.2f", value=9.56)
 div = col1.number_input("2023 Dividends (billion $)", min_value=0.0, format="%.2f", value=5.87)
 mkt = col1.number_input("2023 Market Capitalization (billion $)", min_value=0.0, format="%.2f", value=68.4)
@@ -16,8 +16,8 @@ current_price = col1.number_input("Current Price", min_value=0.0, format="%.2f",
 
 rev_h5 = col2.number_input("Revenue growth in next 5 years (%)", min_value=0.0, format="%.2f", value=3.0)
 rev_h10 = col2.number_input("Revenue growth in 5 to 10 years (%)", min_value=0.0, format="%.2f", value=3.0)
-fcf_m = col2.number_input("FCF margin", min_value=0.0, format="%.2f", value=4.0)
-r = col2.number_input("Discount rate", min_value=0.0, format="%.2f", value=10.0)
+fcf_m = col2.number_input("FCF margin (%)", min_value=0.0, format="%.2f", value=4.0)
+r = col2.number_input("Discount rate (%)", min_value=0.0, format="%.2f", value=10.0)
 t_m = col2.number_input("Terminal multiple", min_value=0.0, format="%.2f", value=9.0)
 
 # Create a data-frame with the REV-FCF-Div schedule.
