421bf1c80feaf56b8a6343404be7eb8a9dee7af3
giacbracci <154382061+giacbracci@users.noreply.github.com>
2023-12-22 18:11:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,19 +30,19 @@ for i in range(1, 11):
     if i < 6:
         rev_f = rev_start * (1+(rev_h5/100))
         fcf_f = rev_f * (fcf_m/100)
-        div_s = fcf_f * ((1+(r/100))^(i-2))
+        div_s = fcf_f * ((1+(r/100))**(i-2))
         div_f = div_s * div_fcf
         rev_start = rev_f
     elif i > 6 and i < 11:
         rev_f = rev_start * (1+(rev_h5/100))
         fcf_f = rev_f * (fcf_m/100)
-        div_s = fcf_f * ((1+(r/100))^(i-2))
+        div_s = fcf_f * ((1+(r/100))**(i-2))
         div_f = div_s * div_fcf
         rev_start = rev_f
     else:
         rev_f = rev_start
         fcf_f = rev_f * t_m
-        div_s = fcf_f * ((1+(r/100))^(i-2))
+        div_s = fcf_f * ((1+(r/100))**(i-2))
         div_f = div_s * div_fcf
         rev_start = rev_f
     schedule.append(
