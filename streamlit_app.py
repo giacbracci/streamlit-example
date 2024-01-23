8d70d2f4a0c21a0cfe5750bd32714df8858da710
giacbracci <154382061+giacbracci@users.noreply.github.com>
2024-01-23 20:21:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,23 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
-import math
+import streamlit as st 
+import requests
 
-st.title("FEF Academy Stock Price Fair Value Calculator")
+#Initial UI
+ticker = st.text_input('Ticker', "NFLX").upper()
+buttonClicked = st.button('Set')
 
-st.write("### Input Data")
-col1, col2 = st.columns(2)
-rev = col1.number_input("2023 Revenues (billion $)", min_value=0.0, format="%.2f", value=228.96)
-fcf = col1.number_input("2023 Free Cash Flow (billion $)", min_value=0.0, format="%.2f", value=9.56)
-div = col1.number_input("2023 Dividends (billion $)", min_value=0.0, format="%.2f", value=5.87)
-mkt = col1.number_input("2023 Market Capitalization (billion $)", min_value=0.0, format="%.2f", value=68.4)
-current_price = col1.number_input("Current Price", min_value=0.0, format="%.2f", value=11.19)
+#Callbacks
+if buttonClicked:
+  requestString = f"""https://query1.finance.yahoo.com/v10/...{ticker}?modules=assetProfile%2Cprice"""
+  request = requests.get(f"{requestString}", headers={"USER-AGENT": "Mozilla/5.0"})
+  json = request.json()
+  data = json["quoteSummary"]["result"][0]
 
-rev_h5 = col2.number_input("Revenue growth in next 5 years (%)", min_value=0.0, format="%.2f", value=3.0)
-rev_h10 = col2.number_input("Revenue growth in 5 to 10 years (%)", min_value=0.0, format="%.2f", value=3.0)
-fcf_m = col2.number_input("FCF margin (%)", min_value=0.0, format="%.2f", value=4.0)
-r = col2.number_input("Discount rate (%)", min_value=0.0, format="%.2f", value=10.0)
-t_m = col2.number_input("Terminal multiple", min_value=0.0, format="%.2f", value=9.0)
+  st.header("Profile")
 
-# Create a data-frame with the REV-FCF-Div schedule.
-schedule = []
-# remaining_balance = loan_amount
-rev_start = rev
-fcf_start = fcf
-div_fcf = div/fcf
+  st.metric("sector", data["assetProfile"]["sector"])
+  st.metric("industry", data["assetProfile"]["industry"])
+  st.metric("website", data["assetProfile"]["website"])
+  st.metric("marketCap", data["price"]["marketCap"]["fmt"])
 
-for i in range(1, 11):
-    if i <= 5:
-        rev_f = rev_start * (1 + (rev_h5 / 100))
-    else:
-        rev_f = rev_start * (1 + (rev_h10 / 100))
-    
-    fcf_f = rev_f * (fcf_m/100)
-    div_s = fcf_f * ((1+(r/100))**(1-i-1))
-    div_f = div_s * div_fcf
-    rev_start = rev_f
-    schedule.append(
-        [
-            i,
-            rev_f,
-            fcf_f,
-            div_f,
-        ]
-    )
-
-df = pd.DataFrame(schedule, columns=["Year", "Revenues", "Free Cash Flow", "Dividends"])
-FCF_t = fcf_f * t_m
-div_t = FCF_t * ((1+(r/100))**(-10))
-
-intrinsic_value = df["Dividends"].sum()+div_t
-price_target = (current_price * intrinsic_value) / mkt
-delta_price = price_target - current_price
-
-st.write("### Results")
-
-st.metric(label="Target Price", value=round(price_target,2), delta=round(delta_price,2))
-
-
+  with st.expander("About Company"):
+    st.write(data["assetProfile"]["longBusinessSummary"])
